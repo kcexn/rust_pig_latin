11425d9123d001f741326dca2e19c87addcd0094
Kevin <068ant@gmail.com>
2018-01-06 15:03:05 +1100

    built unit test for translate_to_pig_latin

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+extern crate regex;
+
+use regex::Regex;
+
+pub struct OriginalWord {
+   pub word: String,
+}
+
+impl OriginalWord {
+    pub fn new(mut args: std::env::Args) -> Result<OriginalWord, &'static str> { 
+        args.next();  
+        
+        let word = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        Ok( OriginalWord{word} )
+    }
+
+    fn get(self) -> String {
+        self.word
+    }
+}
+
+pub fn translate_to_pig_latin(word: OriginalWord) -> String {
+    let original_word = word.get();
+    let re = Regex::new("^[aeiou]").unwrap();
+
+    if re.is_match(&original_word) {
+        return original_word
+    } else {
+        return String::from("atinlay") 
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_translate_to_pig_latin() {
+        // For words that begin with consonant sounds, all letters before the initial vowel are
+        // placed at the end of the word sequence. Then, "ay" is added, as in the following
+        // examples:
+        assert_eq!( String::from("atinlay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("latin") } ));
+        assert_eq!( String::from("ananabay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("banana") } ));
+        assert_eq!( String::from("ashtray"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("trash") } ));
+        assert_eq!( String::from("appyhay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("happy") } ));
+        assert_eq!( String::from("uckday"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("duck") } ));
+        assert_eq!( String::from("opestday"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("dopest") } ));
+        assert_eq!( String::from("emay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("me") } ));
+        assert_eq!( String::from("ootay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("too") } ));
+        assert_eq!( String::from("illway"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("will") } ));
+        assert_eq!( String::from("oistmay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("moist") } ));
+        assert_eq!( String::from("etway"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("wet") } ));
+        assert_eq!( String::from("ealray"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("real") } ));
+        assert_eq!( String::from("implesay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("simple") } ));
+        assert_eq!( String::from("aysay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("say") } ));
+        assert_eq!( String::from("ordsway"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("words") } ));
+        // When words begin with consonant clusters (multiple consonants that form one sound), the
+        // whole sound is added to the end when speaking or writing.
+        assert_eq!( String::from("eerschay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("cheers") } ));
+        assert_eq!( String::from("eshshay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("cheers") } ));
+        assert_eq!( String::from("ilesmay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("smile") } ));
+        assert_eq!( String::from("ingstray"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("string") } ));
+        assert_eq!( String::from("anksthay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("thanks") } ));
+        assert_eq!( String::from("upidstay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("stupid") } ));
+        assert_eq!( String::from("oveglay"), super::translate_to_pig_latin( super:: OriginalWord{ word: String::from("glove") } ));
+        // For words that begin with vowel sounds, one just adds "way" or "yay" to the end (or just
+        // "ay"). Examples are
+        assert_eq!( String::from("eatay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("eat") } ));
+        assert_eq!( String::from("omeletay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("omelete") } ));
+        assert_eq!( String::from("areay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("are") } ));
+        assert_eq!( String::from("eggay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("egg") } ));
+        assert_eq!( String::from("explainay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("explain") } ));
+        assert_eq!( String::from("alwaysay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("always") } ));
+        assert_eq!( String::from("endsay"), super::translate_to_pig_latin( super::OriginalWord{ word: String::from("ends") } ));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate pig_latin;
+
+use std::env;
+use std::process;
+
+use pig_latin::OriginalWord;
+
 fn main() {
-    println!("Hello, world!");
+    let word = OriginalWord::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    pig_latin::translate_to_pig_latin(word);
 }
